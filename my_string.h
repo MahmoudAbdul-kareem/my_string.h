b0536b44705ecb74232edb4d199701ef892bbcda
MahmoudAbdul-kareem <mahmoudabdulkareemawad@gmail.com>
2023-09-12 20:24:38 +0300

    separate my_string.h function by empty lines

--- my_string.h
+++ my_string.h
@@ -19,24 +19,43 @@
 
 /* ~~~~ Functions Prototypes ~~~~ */
 void *my_memchr(const void *const str,const int value, unsigned int n);
+
 int my_memcmp(const void *const str1, const void *const str2, unsigned int n);
+
 void *my_memcpy(void *const dest, const void *const src, const unsigned int n);
+
 void *my_memmove_v1(void *const dest, const void *const src,const unsigned int n);
+
 void *my_memmove_v2(void *const dest, const void *const src,const unsigned int n);
+
 void *my_memset(void *const dest, const int ch, unsigned int n);
+
 char *my_strcat(char *dest, const char *src);
+
 char *my_strncat(char *dest, const char *src, unsigned int n);
+
 char *my_strchr(const char *str, const int c);
+
 int my_strcmp(const char *str1, const char *str2);
+
 int my_strncmp(const char *str1, const char *str2, unsigned int n);
+
 char *my_strcpy(char *dest, const char *src);
+
 char *my_strncpy(char *dest, const char *src, unsigned int n);
+
 unsigned int my_strcspn_v1(const char *str1, const char *const str2);
+
 unsigned int my_strcspn_v2(const char *str1, const char *const str2);
+
 unsigned int my_strlen(const char *str);
+
 char *my_strpbrk(const char *str1, const char *str2);
+
 char *my_strrchr(const char *str, const int c);
+
 unsigned int my_strspn(const char *str1, const char *const str2);
+
 char *my_strstr(const char *haystack, const char *const needle);
 
 #endif
